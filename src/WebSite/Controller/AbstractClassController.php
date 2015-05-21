2557bd9bb921f52cc3ab62062980b2f62d70b1f0
AwHFR <kevin.alberca@supinternet.fr>
2015-05-21 13:04:17 +0200

    Add getUserManager method

--- src/WebSite/Controller/AbstractClassController.php
+++ src/WebSite/Controller/AbstractClassController.php
@@ -9,17 +9,11 @@ class AbstractClassController {
 
     public function getConnexion(){
         $myConfig = Yaml::parse(file_get_contents(__DIR__.'/../../../app/config/config-dev.yml'));
-
-        $config = new \Doctrine\DBAL\Configuration();
-
-        $connectionParams = [
-            'dbname' => $myConfig['doctrine']['database'],
-            'user' => $myConfig['doctrine']['username'],
-            'password' => $myConfig['doctrine']['password'],
-            'host' => $myConfig['doctrine']['host'],
-            'driver' => $myConfig['doctrine']['driver'],
-        ];
-
-        return \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
+        return \Doctrine\DBAL\DriverManager::getConnection($myConfig['doctrine'], new \Doctrine\DBAL\Configuration());
     }
+
+    protected function getUserManager() {
+        return new \Website\Model\UserManager($this->getConnection());
+    }
+
 }
