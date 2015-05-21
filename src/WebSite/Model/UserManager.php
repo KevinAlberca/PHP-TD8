95a54072a8e3b5c74601b7f35e40fe8b1134d4a6
AwHFR <kevin.alberca@supinternet.fr>
2015-05-21 12:47:25 +0200

    #5 - All Method created

--- src/WebSite/Model/UserManager.php
+++ src/WebSite/Model/UserManager.php
@@ -4,4 +4,28 @@ namespace Website\Model;
 
 class UserManager {
 
+    public function __construct(){
+
+    }
+
+    public function listUser(){
+
+    }
+
+    public function showUser(){
+
+    }
+
+    public function addUser(){
+
+    }
+
+    public function logUser(){
+
+    }
+
+    public function deleteUser(){
+
+    }
+
 }
